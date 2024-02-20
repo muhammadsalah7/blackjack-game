73beb117f50cd7b8507ef89824149fd27a3bf461
Muhammad Salah <muhammadsalah217@gmail.com>
2024-02-20 13:47:54 +0200

    added drawing a new card

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.querySelector("#message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`;
+  sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "You've got Blackjack!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
-  console.log(message);
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let newCard = 7;
+  sum += newCard;
+  cards.push(newCard);
+  console.log(cards);
+  renderGame();
 }
